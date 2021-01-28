a2712cd7a72fb83da2c6e7a4faffbc0d28e6fb58
CliffBooth <feraljerboa@gmail.com>
2021-01-28 12:38:45 +0300

    module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module Tetris {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens javaFX to javafx.graphics;
     opens fxml to javafx.fxml;
-    exports javaFX;
     exports fxml;
 }
